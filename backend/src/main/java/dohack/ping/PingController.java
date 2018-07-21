9517fbced50bdfd7bcc2438ec44a2937e80b42ce
Nghiem Phan <nghiemphan93@gmail.com>
2018-07-21 21:10:39 +0200

    fixed seeding

--- backend/src/main/java/dohack/ping/PingController.java
+++ backend/src/main/java/dohack/ping/PingController.java
@@ -1,12 +1,19 @@
 package dohack.ping;
 
+import dohack.seeding.Seeding;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+   @Autowired
+   public Seeding seeding;
+   
    @RequestMapping( value = "/ping")
    public String ping(){
+      seeding.seedData();
+      
       return "ping";
    }
 }
