7e5da654bc5a9a0910fb8e621521248a5173fb7a
Philip Dzierzon <philip.dzierzon@gmail.com>
2018-07-21 14:47:34 +0200

    LoginScreenComponents

---
+++ frontend/src/app/login/authentication-actions/authentication-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication-actions',
+  templateUrl: './authentication-actions.component.html',
+  styleUrls: ['./authentication-actions.component.css']
+})
+export class AuthenticationActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
