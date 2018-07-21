e8dbc29564aba7f05278684cdc261d8199dc54cc
Philip Dzierzon <philip.dzierzon@gmail.com>
2018-07-21 11:00:10 +0200

    initial commit

---
+++ backend/src/main/java/dohack/DohackApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class DohackApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DohackApplication.class, args);
+	}
+
+}
