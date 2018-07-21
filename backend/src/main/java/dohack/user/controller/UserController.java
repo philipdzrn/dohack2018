5dca56c2b9ef2a9c0769147d7f73136d23c336ef
cstockhoff <33723534+cstockhoff@users.noreply.github.com>
2018-07-21 16:43:57 +0200

    fixed error in userController

--- backend/src/main/java/dohack/user/controller/UserController.java
+++ backend/src/main/java/dohack/user/controller/UserController.java
@@ -4,6 +4,7 @@ import dohack.challenge.dto.ChallengeDTO;
 import dohack.challenge.model.Challenge;
 import dohack.challenge.service.ChallengeService;
 import dohack.user.dto.UserDTO;
+import dohack.user.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,22 +23,26 @@ public class UserController {
   @Autowired
   private ChallengeService challengeService;
 
-  @RequestMapping( "/profile" )
+  @Autowired
+  private UserRepository userRepository;
+
+  @RequestMapping( value = "/profile" )
   public UserDTO getUser(@PathVariable("userId") String userId ) {
-    //TODO User-Profil laden
+    //userRepository.f
     UserDTO user = null;
     return user;
   }
 
-  @RequestMapping( "/challenges", method = RequestMethod.GET )
+  @RequestMapping( value = "/challenges", method = RequestMethod.GET )
   public ResponseEntity<List<ChallengeDTO>> getChallenges(@PathVariable("userId") Integer userId ) {
     List<Challenge> challenges = challengeService.getChallengesByUser(userId);
     List<ChallengeDTO> dtoChallenges = new ArrayList<ChallengeDTO>();
-    challenges.forEach( arg  -> {
+    /*challenges.forEach( arg  -> {
             ChallengeDTO dtoChallenge = new ChallengeDTO();
             dtoChallenge.setName(arg.getName());
             dtoChallenges.add( dtoChallenge );
     };
+    */
     return new ResponseEntity( dtoChallenges, HttpStatus.OK );
   }
 }
