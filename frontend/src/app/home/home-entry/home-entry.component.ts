393aa4df49151f139ab7387d856752afba1b91fa
Joshua Hoffmann <joshihoffmann@yahoo.de>
2018-07-22 02:36:33 +0200

    homeEntryUpdate

--- frontend/src/app/home/home-entry/home-entry.component.ts
+++ frontend/src/app/home/home-entry/home-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Challenge} from "../../challenge/challenge";
 
 @Component({
   selector: 'app-home-entry',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-entry.component.css']
 })
 export class HomeEntryComponent implements OnInit {
+
+  challenge:Challenge;
 
   constructor() { }
 
