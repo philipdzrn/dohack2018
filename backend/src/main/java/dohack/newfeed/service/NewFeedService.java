92a1f3cfac9bcbd1aac56744c7eb5bb836d0cbf3
Nghiem Phan <nghiemphan93@gmail.com>
2018-07-22 09:36:47 +0200

    Auto stash before merge of "master" and "origin/master"
    sort new feed

--- backend/src/main/java/dohack/newfeed/service/NewFeedService.java
+++ backend/src/main/java/dohack/newfeed/service/NewFeedService.java
@@ -13,39 +13,38 @@ import java.util.List;
 
 @Service
 public class NewFeedService {
-
-    @Autowired
-    private ChallengeService challengeService;
-
-    @Autowired
-    private UserLikesChallengeService userLikesChallengeService;
-
-    public List<NewFeedDTO> getAllNewFeeds() {
-        // Get all challenges
-        List<Challenge> challenges = this.challengeService.getAllChallenges();
-
-        // Get all challenge DTOs
-        List<ChallengeDTO> challengeDTOs = this.challengeService.getAllChallengeDTOs(challenges);
-
-        // Create new feed list to return
-        List<NewFeedDTO> newFeeds = new ArrayList<>();
-
-        for (int i = 0; i < challenges.size(); i++) {
-            // Get like number by challenge
-            int likeNumber = this.userLikesChallengeService.getLikesByChallenge(challenges.get(i));
-
-            // Create a NewFeedDTO
-            NewFeedDTO newFeedDTO = new NewFeedDTO();
-            newFeedDTO.setChallengeDTO(challengeDTOs.get(i));
-            newFeedDTO.setLikesNumber(likeNumber);
-
-            // Push the object to newFeeds list
-            newFeeds.add(newFeedDTO);
-
-        }
-
-        newFeeds.sort((a, b) -> b.getChallengeDTO().updatedAt.compareTo(a.getChallengeDTO().updatedAt));
-
-        return newFeeds;
-    }
+   
+   @Autowired
+   private ChallengeService challengeService;
+   
+   @Autowired
+   private UserLikesChallengeService userLikesChallengeService;
+   
+   public List<NewFeedDTO> getAllNewFeeds() {
+      // Get all challenges
+      List<Challenge> challenges = this.challengeService.getAllChallenges();
+      
+      // Get all challenge DTOs
+      List<ChallengeDTO> challengeDTOs = this.challengeService.getAllChallengeDTOs(challenges);
+      
+      // Create new feed list to return
+      List<NewFeedDTO> newFeeds = new ArrayList<>();
+      
+      for (int i = 0; i < challenges.size(); i++) {
+         // Get like number by challenge
+         int likeNumber = this.userLikesChallengeService.getLikesByChallenge(challenges.get(i));
+         
+         // Create a NewFeedDTO
+         NewFeedDTO newFeedDTO = new NewFeedDTO();
+         newFeedDTO.setChallengeDTO(challengeDTOs.get(i));
+         newFeedDTO.setLikesNumber(likeNumber);
+         
+         // Push the object to newFeeds list
+         newFeeds.add(newFeedDTO);
+      }
+      
+      newFeeds.sort((a,b) -> b.getChallengeDTO().updatedAt.compareTo(a.getChallengeDTO().updatedAt));
+      
+      return newFeeds;
+   }
 }
